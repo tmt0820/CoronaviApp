0c0261f52f1615817ef797bca739248baf370545
tmt0820 <tmt08200@gmail.com>
2023-02-16 18:45:40 +0900

    Initial Commit

---
+++ CoronaviApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoronaviApp
+//
+//  Created by 三澤俊裕 on 2023/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
